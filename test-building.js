c0668a755456fb91335dc2eb0cd02a747a7eb98e
code not my father <84304475+idontknowtoobrother@users.noreply.github.com>
2022-03-01 21:12:14 +0700

    added: new 2 more rooms

--- test-building.js
+++ test-building.js
@@ -7,14 +7,25 @@ const Building = require('./models/Building')
 
 async function main(){
     const informaticsBuilding = new Building({name: 'Informatics', floor: 11})
+    const newInformaticsBuilding = new Building({name: 'New Informatics', floor: 20})
+    
+    // 3C01, 4C01, 5C01
     const room3c01 = new Room({ name: '3c01', capacity: 200, floor: 3, building: informaticsBuilding})
+    const room4c01 = new Room({ name: '4c01', capacity: 200, floor: 4, building: informaticsBuilding})
+    const room5c01 = new Room({ name: '5c01', capacity: 200, floor: 5, building: informaticsBuilding})
+    
+    // Push to building room
     informaticsBuilding.rooms.push(room3c01)
-    informaticsBuilding.save().then(()=>{
-        console.log('"Informatics" Building saved !')
-    })
-    room3c01.save().then(()=>{
-        console.log('"3C01" Room saved !')
-    })
+    informaticsBuilding.rooms.push(room4c01)
+    informaticsBuilding.rooms.push(room5c01)
+
+    // Save All
+    await informaticsBuilding.save()
+    await newInformaticsBuilding.save()
+    await room3c01.save()
+    await room4c01.save()
+    await room5c01.save()
+
 }
 
 
